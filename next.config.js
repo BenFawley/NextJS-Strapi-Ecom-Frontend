36de92b5ebad2d01a0edcc3c0cf7038be7a7e0e5
BenFawley <60470850+BenFawley@users.noreply.github.com>
2023-08-08 08:19:34 +0100

    Allow image domain for Strapi API

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["127.0.0.1", "https://exhibit-ecommerce.netlify.app"],
+    domains: ["127.0.0.1", "https://exhibit-ecommerce.netlify.app", "https://exhibit-mcj9s.ondigitalocean.app/"],
   },
 };
 
