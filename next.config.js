60c96df5c30a64375b8e07596d1d301c58c24aca
BenFawley <60470850+BenFawley@users.noreply.github.com>
2023-08-08 09:02:59 +0100

    allow extenal URL for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     domains: ["127.0.0.1", "netlify.app", "ondigitalocean.app"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'exhibit-mcj9s.ondigitalocean.app',
+        port: '',
+        pathname: '/uploads/**',
+      },
+    ],
   },
 };
 
